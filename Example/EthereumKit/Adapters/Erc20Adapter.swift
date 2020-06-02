d53efa05ab71282d04aae6b4990eaff2606b7343
Ermat <ealymbaev@gmail.com>
2020-06-02 16:50:04 +0600

    Make address validation in EthereumKit static

--- Example/EthereumKit/Adapters/Erc20Adapter.swift
+++ Example/EthereumKit/Adapters/Erc20Adapter.swift
@@ -121,7 +121,7 @@ extension Erc20Adapter: IAdapter {
     }
 
     func validate(address: String) throws {
-        try ethereumKit.validate(address: address)
+        try EthereumKit.Kit.validate(address: address)
     }
 
     func sendSingle(to: String, amount: Decimal, gasLimit: Int) -> Single<Void> {
