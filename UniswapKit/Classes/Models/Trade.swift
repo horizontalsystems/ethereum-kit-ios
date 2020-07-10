a05d1e418d2192f7dd05fdd29eb1d74641c84cab
Ermat <ealymbaev@gmail.com>
2020-07-10 18:48:05 +0600

    Create TradeData model for holding Trade and TradeOptions

--- UniswapKit/Classes/Models/Trade.swift
+++ UniswapKit/Classes/Models/Trade.swift
@@ -1,35 +1,6 @@
-import BigInt
-
-public struct Trade {
-    public let type: TradeType
+struct Trade {
+    let type: TradeType
     let route: Route
     let tokenAmountIn: TokenAmount
     let tokenAmountOut: TokenAmount
-
-    private func amountInMax(slippage: Double) -> BigUInt {
-        tokenAmountIn.amount * (100_00 + BigUInt(slippage * 100)) / 100_00
-    }
-
-    private func amountOutMin(slippage: Double) -> BigUInt {
-        tokenAmountOut.amount * (100_00 - BigUInt(slippage * 100)) / 100_00
-    }
-
-    public var amountIn: String {
-        tokenAmountIn.amount.description
-    }
-
-    public var amountOut: String {
-        tokenAmountOut.amount.description
-    }
-
-    public func amountInMax(slippage: Double) -> String {
-        let value: BigUInt = amountInMax(slippage: slippage)
-        return value.description
-    }
-
-    public func amountOutMin(slippage: Double) -> String {
-        let value: BigUInt = amountOutMin(slippage: slippage)
-        return value.description
-    }
-
 }
