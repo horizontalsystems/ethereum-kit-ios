da82fa8f229dee7ef3ab5305380a188636eb0210
Ermat <ealymbaev@gmail.com>
2020-07-22 18:32:45 +0600

    Remove NetworkType from UniswapKit constructor and use it from EthereumKit

--- Example/EthereumKit/Core/Manager.swift
+++ Example/EthereumKit/Core/Manager.swift
@@ -59,7 +59,7 @@ class Manager {
                 minLogLevel: configuration.minLogLevel
         )
 
-        uniswapKit = try! UniswapKit.Kit.instance(ethereumKit: ethereumKit, networkType: configuration.networkType)
+        uniswapKit = try! UniswapKit.Kit.instance(ethereumKit: ethereumKit)
 
         ethereumAdapter = EthereumAdapter(ethereumKit: ethereumKit)
 
