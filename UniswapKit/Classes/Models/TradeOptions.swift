391d1dc8ebf674c88991cd4982bb258fde250c65
anton <ant013@mail.ru>
2020-11-09 18:31:48 +0600

    make defaults for TradeOptions.

--- UniswapKit/Classes/Models/TradeOptions.swift
+++ UniswapKit/Classes/Models/TradeOptions.swift
@@ -1,12 +1,15 @@
 import EthereumKit
 
 public struct TradeOptions {
+    public static let defaultSlippage: Decimal = 0.5
+    public static let defaultTtl: TimeInterval = 20 * 60
+
     public var allowedSlippage: Decimal
     public var ttl: TimeInterval
     public var recipient: Address?
     public var feeOnTransfer: Bool
 
-    public init(allowedSlippage: Decimal = 0.5, ttl: TimeInterval = 20 * 60, recipient: Address? = nil, feeOnTransfer: Bool = false) {
+    public init(allowedSlippage: Decimal = TradeOptions.defaultSlippage, ttl: TimeInterval = TradeOptions.defaultTtl, recipient: Address? = nil, feeOnTransfer: Bool = false) {
         self.allowedSlippage = allowedSlippage
         self.ttl = ttl
         self.recipient = recipient
