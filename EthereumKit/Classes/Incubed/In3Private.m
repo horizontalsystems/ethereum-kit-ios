d6c7e27274e5e8c963e6ed81b8a2ee0e8c726fa8
Ermat <ealymbaev@gmail.com>
2021-04-20 17:49:38 +0600

    Temporarily remove incubed libraries from EthereumKit pod

--- EthereumKit/Classes/Incubed/In3Private.m
+++ EthereumKit/Classes/Incubed/In3Private.m
@@ -1,101 +1,101 @@
-#import "In3Private.h"
-#import <incubed/eth_api.h> // functions for direct api-access
-#import <incubed/in3_init.h> // if included the verifier will automaticly be initialized.
-
-@implementation In3Private
-in3_t *client;
-
-NSString *const GET_BLOCK_BY_NUMBER                         = @"eth_getBlockByNumber";
-NSString *const BLOCK_BY_HASH                               = @"eth_getBlockByHash";
-NSString *const BLOCK_NUMBER                                = @"eth_blockNumber";
-NSString *const GAS_PRICE                                   = @"eth_gasPrice";
-NSString *const CHAIN_ID                                    = @"eth_chainId";
-NSString *const CALL                                        = @"eth_call";
-NSString *const ESTIMATE_GAS                                = @"eth_estimateGas";
-NSString *const GET_BALANCE                                 = @"eth_getBalance";
-NSString *const GET_CODE                                    = @"eth_getCode";
-NSString *const GET_STORAGE_AT                              = @"eth_getStorageAt";
-NSString *const GET_BLOCK_TRANSACTION_COUNT_BY_HASH         = @"eth_getBlockTransactionCountByHash";
-NSString *const GET_BLOCK_TRANSACTION_COUNT_BY_NUMBER       = @"eth_getBlockTransactionCountByNumber";
-NSString *const GET_FILTER_CHANGES                          = @"eth_getFilterChanges";
-NSString *const GET_FILTER_LOGS                             = @"eth_getFilterLogs";
-NSString *const GET_LOGS                                    = @"eth_getLogs";
-NSString *const GET_TRANSACTION_BY_BLOCK_HASH_AND_INDEX     = @"eth_getTransactionByBlockHashAndIndex";
-NSString *const GET_TRANSACTION_BY_BLOCK_NUMBER_AND_INDEX   = @"eth_getTransactionByBlockNumberAndIndex";
-NSString *const GET_TRANSACTION_BY_HASH                     = @"eth_getTransactionByHash";
-NSString *const GET_TRANSACTION_COUNT                       = @"eth_getTransactionCount";
-NSString *const GET_TRANSACTION_RECEIPT                     = @"eth_getTransactionReceipt";
-NSString *const GET_UNCLE_BY_BLOCK_NUMBER_AND_INDEX         = @"eth_getUncleByBlockNumberAndIndex";
-NSString *const GET_UNCLE_COUNT_BY_BLOCK_HASH               = @"eth_getUncleCountByBlockHash";
-NSString *const GET_UNCLE_COUNT_BY_BLOCK_NUMBER             = @"eth_getUncleCountByBlockNumber";
-NSString *const NEW_BLOCK_FILTER                            = @"eth_newBlockFilter";
-NSString *const NEW_FILTER                                  = @"eth_newFilter";
-NSString *const UNINSTALL_FILTER                            = @"eth_uninstallFilter";
-NSString *const SEND_RAW_TRANSACTION                        = @"eth_sendRawTransaction";
-NSString *const SEND_TRANSACTION                            = @"eth_sendTransaction";
-NSString *const ABI_ENCODE                                  = @"in3_abiEncode";
-NSString *const ABI_DECODE                                  = @"in3_abiDecode";
-NSString *const CHECKSUM_ADDRESS                            = @"in3_checksumAddress";
-NSString *const ENS                                         = @"in3_ens";
-
-- (instancetype)initWithChainId:(UInt32)chainId {
-    client = in3_for_chain(chainId);
-    client->proof = PROOF_NONE;
-
-    return self;
-}
-
-- (UInt64)blockNumber {
-    return eth_blockNumber(client);
-}
-
-- (UInt64)transactionCount:(NSData *)address {
-    return eth_getTransactionCount(client, (uint8_t *)address.bytes, BLKNUM_LATEST());
-}
-
-- (NSNumber *)transactionReceipt:(NSData *)transactionHash {
-    eth_tx_receipt_t *receipt = eth_getTransactionReceipt(client, (uint8_t *)transactionHash.bytes);
-    if (receipt != nil) {
-        return receipt->status ? @1 : @0;
-    }
-    return @-1;
-}
-
-- (bool)transactionExist:(NSData *)transactionHash {
-    eth_tx_receipt_t *receipt = eth_getTransactionReceipt(client, (uint8_t *)transactionHash.bytes);
-    return receipt != nil;
-}
-
-- (NSString *)rpcCall:(NSString *)method params:(NSString *)parameters didFailWithError:(NSError **)error {
-    char *call_method = (char *) [method UTF8String];
-    char *call_params = (char *) [parameters UTF8String];
-
-    // prepare 2 pointers for the result.
-    char *cResult, *cError;
-    in3_ret_t res = in3_client_rpc(
-            client,                                                         //  the configured client
-            call_method,                                                    // the rpc-method you want to call.
-            call_params,                                                    // the arguments as json-string
-            &cResult,                                                       // the reference to a pointer will hold the result
-            &cError);                                                       // the pointer which may hold a error message
-
-    if (res == IN3_OK) {
-        NSString *result = [NSString stringWithUTF8String: cResult];
-
-        free(cResult);
-        return result;
-    } else {
-        *error = [self error: [NSString stringWithUTF8String: cError]];
-        free(cError);
-        return NULL;
-    }
-
-}
-
-// ###################
-
-- (NSError *)error:(NSString *)text {
-    return [[NSError alloc] initWithDomain: @"incubed.wrapper" code: 101 userInfo: @{NSLocalizedDescriptionKey: text}];
-}
-
-@end
+//#import "In3Private.h"
+//#import <incubed/eth_api.h> // functions for direct api-access
+//#import <incubed/in3_init.h> // if included the verifier will automaticly be initialized.
+//
+//@implementation In3Private
+//in3_t *client;
+//
+//NSString *const GET_BLOCK_BY_NUMBER                         = @"eth_getBlockByNumber";
+//NSString *const BLOCK_BY_HASH                               = @"eth_getBlockByHash";
+//NSString *const BLOCK_NUMBER                                = @"eth_blockNumber";
+//NSString *const GAS_PRICE                                   = @"eth_gasPrice";
+//NSString *const CHAIN_ID                                    = @"eth_chainId";
+//NSString *const CALL                                        = @"eth_call";
+//NSString *const ESTIMATE_GAS                                = @"eth_estimateGas";
+//NSString *const GET_BALANCE                                 = @"eth_getBalance";
+//NSString *const GET_CODE                                    = @"eth_getCode";
+//NSString *const GET_STORAGE_AT                              = @"eth_getStorageAt";
+//NSString *const GET_BLOCK_TRANSACTION_COUNT_BY_HASH         = @"eth_getBlockTransactionCountByHash";
+//NSString *const GET_BLOCK_TRANSACTION_COUNT_BY_NUMBER       = @"eth_getBlockTransactionCountByNumber";
+//NSString *const GET_FILTER_CHANGES                          = @"eth_getFilterChanges";
+//NSString *const GET_FILTER_LOGS                             = @"eth_getFilterLogs";
+//NSString *const GET_LOGS                                    = @"eth_getLogs";
+//NSString *const GET_TRANSACTION_BY_BLOCK_HASH_AND_INDEX     = @"eth_getTransactionByBlockHashAndIndex";
+//NSString *const GET_TRANSACTION_BY_BLOCK_NUMBER_AND_INDEX   = @"eth_getTransactionByBlockNumberAndIndex";
+//NSString *const GET_TRANSACTION_BY_HASH                     = @"eth_getTransactionByHash";
+//NSString *const GET_TRANSACTION_COUNT                       = @"eth_getTransactionCount";
+//NSString *const GET_TRANSACTION_RECEIPT                     = @"eth_getTransactionReceipt";
+//NSString *const GET_UNCLE_BY_BLOCK_NUMBER_AND_INDEX         = @"eth_getUncleByBlockNumberAndIndex";
+//NSString *const GET_UNCLE_COUNT_BY_BLOCK_HASH               = @"eth_getUncleCountByBlockHash";
+//NSString *const GET_UNCLE_COUNT_BY_BLOCK_NUMBER             = @"eth_getUncleCountByBlockNumber";
+//NSString *const NEW_BLOCK_FILTER                            = @"eth_newBlockFilter";
+//NSString *const NEW_FILTER                                  = @"eth_newFilter";
+//NSString *const UNINSTALL_FILTER                            = @"eth_uninstallFilter";
+//NSString *const SEND_RAW_TRANSACTION                        = @"eth_sendRawTransaction";
+//NSString *const SEND_TRANSACTION                            = @"eth_sendTransaction";
+//NSString *const ABI_ENCODE                                  = @"in3_abiEncode";
+//NSString *const ABI_DECODE                                  = @"in3_abiDecode";
+//NSString *const CHECKSUM_ADDRESS                            = @"in3_checksumAddress";
+//NSString *const ENS                                         = @"in3_ens";
+//
+//- (instancetype)initWithChainId:(UInt32)chainId {
+//    client = in3_for_chain(chainId);
+//    client->proof = PROOF_NONE;
+//
+//    return self;
+//}
+//
+//- (UInt64)blockNumber {
+//    return eth_blockNumber(client);
+//}
+//
+//- (UInt64)transactionCount:(NSData *)address {
+//    return eth_getTransactionCount(client, (uint8_t *)address.bytes, BLKNUM_LATEST());
+//}
+//
+//- (NSNumber *)transactionReceipt:(NSData *)transactionHash {
+//    eth_tx_receipt_t *receipt = eth_getTransactionReceipt(client, (uint8_t *)transactionHash.bytes);
+//    if (receipt != nil) {
+//        return receipt->status ? @1 : @0;
+//    }
+//    return @-1;
+//}
+//
+//- (bool)transactionExist:(NSData *)transactionHash {
+//    eth_tx_receipt_t *receipt = eth_getTransactionReceipt(client, (uint8_t *)transactionHash.bytes);
+//    return receipt != nil;
+//}
+//
+//- (NSString *)rpcCall:(NSString *)method params:(NSString *)parameters didFailWithError:(NSError **)error {
+//    char *call_method = (char *) [method UTF8String];
+//    char *call_params = (char *) [parameters UTF8String];
+//
+//    // prepare 2 pointers for the result.
+//    char *cResult, *cError;
+//    in3_ret_t res = in3_client_rpc(
+//            client,                                                         //  the configured client
+//            call_method,                                                    // the rpc-method you want to call.
+//            call_params,                                                    // the arguments as json-string
+//            &cResult,                                                       // the reference to a pointer will hold the result
+//            &cError);                                                       // the pointer which may hold a error message
+//
+//    if (res == IN3_OK) {
+//        NSString *result = [NSString stringWithUTF8String: cResult];
+//
+//        free(cResult);
+//        return result;
+//    } else {
+//        *error = [self error: [NSString stringWithUTF8String: cError]];
+//        free(cError);
+//        return NULL;
+//    }
+//
+//}
+//
+//// ###################
+//
+//- (NSError *)error:(NSString *)text {
+//    return [[NSError alloc] initWithDomain: @"incubed.wrapper" code: 101 userInfo: @{NSLocalizedDescriptionKey: text}];
+//}
+//
+//@end
--- EthereumKit/Classes/Incubed/RpcParamsHelper.swift
+++ EthereumKit/Classes/Incubed/RpcParamsHelper.swift
@@ -1,59 +1,59 @@
-import Foundation
-import BigInt
-
-class RpcParamsHelper {
-
-    static func dropQuotes(text: String) -> String {
-        var result = text
-        if result.hasPrefix("\"") {
-            result.removeFirst()
-        }
-        if result.hasSuffix("\"") {
-            result.removeLast()
-        }
-        return result
-    }
-
-    private static func dropHex(text: String) -> String {
-        text.hasPrefix("0x") ? String(text.dropFirst(2)) : text 
-    }
-
-    private static func map(_ any: Any) -> Any? {
-        switch any {
-        case let int as Int: return "0x\(String(int, radix: 16))"
-        case let dictionary as [String: Any]: return convert(dictionary)
-        case let array as [Any]: return convert(array)
-        case let data as Data: return data.toHexString()
-        case let bigInt as BigUInt: return bigInt.serialize().toHexString()
-        case Optional<Any>.none: return nil
-        default: return any
-        }
-    }
-
-    static func convert(_ array: [Any]) -> [Any] {
-        array.compactMap(map)
-    }
-
-    static func convert(_ dictionary: [String: Any]) -> [String: Any] {
-        dictionary.compactMapValues(map)
-    }
-
-    static func convert<T: Collection>(_ json: String) throws -> T? {
-        guard let data = json.data(using: .utf8) else {
-            return nil
-        }
-        return try JSONSerialization.jsonObject(with: data, options: []) as? T
-    }
-
-
-    static func convert<T: FixedWidthInteger>(_ json: String) -> T? {
-        let numberString = dropHex(text: json)
-        return T(numberString, radix: 16)
-    }
-
-    static func convert(_ json: String) -> BigUInt? {
-        let numberString = dropHex(text:json)
-        return BigUInt(numberString, radix: 16)
-    }
-
-}
+//import Foundation
+//import BigInt
+//
+//class RpcParamsHelper {
+//
+//    static func dropQuotes(text: String) -> String {
+//        var result = text
+//        if result.hasPrefix("\"") {
+//            result.removeFirst()
+//        }
+//        if result.hasSuffix("\"") {
+//            result.removeLast()
+//        }
+//        return result
+//    }
+//
+//    private static func dropHex(text: String) -> String {
+//        text.hasPrefix("0x") ? String(text.dropFirst(2)) : text
+//    }
+//
+//    private static func map(_ any: Any) -> Any? {
+//        switch any {
+//        case let int as Int: return "0x\(String(int, radix: 16))"
+//        case let dictionary as [String: Any]: return convert(dictionary)
+//        case let array as [Any]: return convert(array)
+//        case let data as Data: return data.toHexString()
+//        case let bigInt as BigUInt: return bigInt.serialize().toHexString()
+//        case Optional<Any>.none: return nil
+//        default: return any
+//        }
+//    }
+//
+//    static func convert(_ array: [Any]) -> [Any] {
+//        array.compactMap(map)
+//    }
+//
+//    static func convert(_ dictionary: [String: Any]) -> [String: Any] {
+//        dictionary.compactMapValues(map)
+//    }
+//
+//    static func convert<T: Collection>(_ json: String) throws -> T? {
+//        guard let data = json.data(using: .utf8) else {
+//            return nil
+//        }
+//        return try JSONSerialization.jsonObject(with: data, options: []) as? T
+//    }
+//
+//
+//    static func convert<T: FixedWidthInteger>(_ json: String) -> T? {
+//        let numberString = dropHex(text: json)
+//        return T(numberString, radix: 16)
+//    }
+//
+//    static func convert(_ json: String) -> BigUInt? {
+//        let numberString = dropHex(text:json)
+//        return BigUInt(numberString, radix: 16)
+//    }
+//
+//}
