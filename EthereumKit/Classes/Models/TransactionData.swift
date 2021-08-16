cf1170162b1adaaf5963c9464af7f337f6217c5f
anton <ant013@mail.ru>
2021-08-16 17:40:00 +0600

    Add option nonce to TransactionData

--- EthereumKit/Classes/Models/TransactionData.swift
+++ EthereumKit/Classes/Models/TransactionData.swift
@@ -4,18 +4,20 @@ public struct TransactionData {
     public var to: Address
     public var value: BigUInt
     public var input: Data
+    public var nonce: Int?
     
-    public init(to: Address, value: BigUInt, input: Data) {
+    public init(to: Address, value: BigUInt, input: Data, nonce: Int? = nil) {
         self.to = to
         self.value = value
         self.input = input
+        self.nonce = nonce
     }
 }
 
 extension TransactionData: Equatable {
 
     public static func ==(lhs: TransactionData, rhs: TransactionData) -> Bool {
-        lhs.to == rhs.to && lhs.value == rhs.value && lhs.input == rhs.input
+        lhs.to == rhs.to && lhs.value == rhs.value && lhs.input == rhs.input && lhs.nonce == rhs.nonce
     }
 
 }
