248c1a6637965b5fdc503c7d54a459dcf7fdce3a
Ermat <ealymbaev@gmail.com>
2022-05-12 15:03:31 +0600

    EthereumKit: fix detecting Contract Creation decoration

--- EthereumKit/Classes/Decorations/EthereumDecorator.swift
+++ EthereumKit/Classes/Decorations/EthereumDecorator.swift
@@ -12,7 +12,7 @@ class EthereumDecorator {
 extension EthereumDecorator: ITransactionDecorator {
 
     public func decoration(from: Address?, to: Address?, value: BigUInt?, contractMethod: ContractMethod?, internalTransactions: [InternalTransaction], eventInstances: [ContractEventInstance]) -> TransactionDecoration? {
-        guard let from = from, let value = value, let contractMethod = contractMethod else {
+        guard let from = from, let value = value else {
             return nil
         }
 
@@ -20,16 +20,14 @@ extension EthereumDecorator: ITransactionDecorator {
             return ContractCreationDecoration()
         }
 
-        guard contractMethod is EmptyMethod else {
-            return nil
-        }
+        if let contractMethod = contractMethod, contractMethod is EmptyMethod {
+            if from == address {
+                return OutgoingDecoration(to: to, value: value, sentToSelf: to == address)
+            }
 
-        if from == address {
-            return OutgoingDecoration(to: to, value: value, sentToSelf: to == address)
-        }
-
-        if to == address {
-            return IncomingDecoration(from: from, value: value)
+            if to == address {
+                return IncomingDecoration(from: from, value: value)
+            }
         }
 
         return nil
